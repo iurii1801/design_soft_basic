712f3e19f4d7a7dd18d0f72ccd66c43e95668e2b
Iurii Bogdanov <iurcikbogdanov18@gmail.com>
2025-09-22 20:34:26 +0300

    labs_basic

---
+++ lab2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Hello, Jerry");
+        Console.WriteLine("--------------------");
+
+        for (int i = 0; i < 3; i++)
+        {
+            ABC();
+            Thread.Sleep(500); 
+        }
+
+        Console.WriteLine("--------------------");
+
+        A();
+        A();
+
+    }
+
+    static void ABC()
+    {
+        Console.WriteLine("A");
+        Console.WriteLine("B");
+        Console.WriteLine("C");
+    }
+
+    static void A()
+    {
+        Console.WriteLine("A");
+        B();
+        C();
+    }
+
+    static void B()
+    {
+        Console.WriteLine("B");
+    }
+
+    static void C()
+    {
+        Console.WriteLine("C");
+    }
+}
