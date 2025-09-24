f52f1b164ccd50cf369603dd3ab64a505dad8f5c
Iurii Bogdanov <145650269+iurii1801@users.noreply.github.com>
2025-09-24 14:42:23 +0300

    labs_basic

---
+++ lab4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 5;
+int b = 6;
+a = b;
+b = 7;
+Console.WriteLine(a);
+a =8;
+Console.WriteLine(b);
+
+int a = 5;
+ int b = a + 6;
+ a = 7;
+ Console.WriteLine(b);
+ Console.WriteLine(b);
+ Console.WriteLine(b);
+ Console.WriteLine(b);
+ Console.WriteLine(b);
+ Console.WriteLine(b);
+ Console.WriteLine(b);
+
+string a = "a";
+string b = a;
+a = "b";
+Console.WriteLine(a);
+
+string a = 5;
